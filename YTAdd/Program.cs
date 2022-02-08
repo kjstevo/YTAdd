7b5147f03a5bd503be4b09fe84af3de3b4749db4
KJ Stevo <kjstevokjmaster@gmail.com>
2022-02-07 22:37:02 -0800

    Turns file names from directory into youtube links

---
+++ YTAdd/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+foreach (string dir in Directory.EnumerateDirectories(@"D:\Music Videos"))
+{
+
+
+    List<string> fileList = Directory.EnumerateFiles(dir).ToList();
+
+    foreach (string file in fileList)
+    {
+        if (file.Contains("["))
+        {
+            var regex = new Regex(@"\[+(\w{11})\]?]");
+            regex.Replace("[", "");
+            regex.Replace("]", "");
+            
+            string id = ((file.Substring(file.IndexOfAny(("[".ToCharArray())))).Split(".")[0]);
+            //regex.Match((id));
+     //       Console.WriteLine(id + " had these items:");
+     foreach (var item in regex.Matches(id) )
+            {
+              //  Console.WriteLine("https://www.youtube.com/watch?v=" + ((string) item).Replace("[","").Replace("]",""));
+            }
+
+     if (regex.Match(id).Success)
+     {
+       string tmp=regex.Match(id).Value;
+       tmp=tmp.Substring(1, 11);
+       Console.WriteLine("https://www.youtube.com/watch?v="+tmp);
+     }
+         //  Console.WriteLine(regex.Match(id));
+        }
+    }
+}
